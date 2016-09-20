715e2628a14f7f1d82c6dc35c9a8302ec79f3ebd
daniellasheva <daniellasheva@gmail.com>
2016-09-20 09:35:29 -0400

    passing tests

--- crewMember.js
+++ crewMember.js
@@ -1,1 +1,36 @@
+function CrewMember(position){
+  this.position = position;
+  this.currentShip="Looking for a Rig";
+}
 
+
+//currentship is a string called 'looking for a rig'. You can't set a property of that
+//string
+
+  CrewMember.prototype.engageWarpDrive = function(){
+    if (this.position==="Pilot" && this.currentShip!= "Looking for a Rig")  {
+      this.currentShip.warpDrive = "engaged!"
+    } else {
+    return "had no effect"
+    }
+  }
+
+  CrewMember.prototype.setsInvisibility = function(){
+    if (this.position==="Defender" && this.currentShip!= "Looking for a Rig") {
+      this.currentShip.cloaked = true
+    } else{
+    return "had no effect"
+    }
+  }
+
+  CrewMember.prototype.chargePhasers = function(){
+    if (this.position==="Gunner" && this.currentShip!= "Looking for a Rig") {
+      this.currentShip.phasersCharge = "charged!"
+    } else {
+    return "had no effect"
+    }
+  }
+
+// CrewMember.prototype = Object.create(Spaceship.prototype)
+//
+// Crewmember.prototype.constructor= Crewmember
---
+++ spaceship.js
@@ -0,0 +1,32 @@
+function Spaceship(name, crew, phasers, shields){
+  this.name=name;
+
+  this.crew =crew
+  this.phasers=phasers;
+  this.shields=shields;
+  this.cloaked = false;
+
+
+  this.warpDrive= "disengaged";
+
+    if (crew.length===0){
+      this.docked = true }
+    else {
+      this.docked=false
+    }
+  this.phasersCharge="uncharged";
+  var ship= this;
+
+    crew.forEach(function(CrewMember){
+      CrewMember.currentShip= ship;
+    })
+}
+
+  // class Spaceship
+  //
+  //   def initialize(crew)
+  //     crew.each do |member|
+  //     member.currenship = self
+  //     end
+  //   end
+  // end
